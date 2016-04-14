aa5fce578ddff3a388e49571f4779b99231fc9f5
paure <paure@192.168.82.191>
2016-04-14 12:26:39 +0200

    He afegit el mateix comentari a tots als mòduls

--- src/projecteprogramaciolmp/Partida.java
+++ src/projecteprogramaciolmp/Partida.java
@@ -5,10 +5,49 @@
  */
 package projecteprogramaciolmp;
 
-/**
- *
- * @author aerop
- */
 public class Partida {
+    // Atributs
+    // --------------------
+    
+    
+    
+    // --------------------
+    
+    // ============================================================
+    // Mètodes CONSULTORS
+    // ============================================================
+        public void vector<Jugador> buscarJugadorMesRic () {
+        // Pre: --
+        // Post: Retorna un vector amb el/s jugador/s més rics
+            
+        }
+        
+        public boolean partidaAcabada () {
+        // Pre: --
+        // Post: Retorna TRUE si la partida s'ha acabat (algun jugador ja arribat al màxim de monedes per guanyar o pel
+        // contrari, algun altre se'n ha quedar sense
+            boolean acabada = false;
+            
+            return acabada;
+        }
+    
+    // ============================================================
+    // Mètodes MODIFICADORS
+    // ============================================================
+        public void repartirCartes () {
+        // Pre: --
+        // Post: Reparteix les cartes als diferents jugadors de la partida
+            
+        }
+
+        private void descartarCartes () {
+        // Pre: --
+        // Post: 
+        }
+
+        public void pagarMulta () {
+
+        }
+    
     
 }
