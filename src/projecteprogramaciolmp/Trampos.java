91fa39a27efc9f9f5e7e41a834bc80f34220280c
paure <paure@192.168.82.191>
2016-04-14 13:07:49 +0200

    He afegit el mateix comentari a tots als mòduls

--- src/projecteprogramaciolmp/Trampos.java
+++ src/projecteprogramaciolmp/Trampos.java
@@ -1,8 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/////////////////////////////////////
+//
+//  Rol Trampós --
+//  El Trampós, quan realitza la seva acció de rol, 
+//
+/////////////////////////////////////
+
 package projecteprogramaciolmp;
 
 /**
