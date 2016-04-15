2bb5bb3c5a435b100f9fa85d42156b8e47e5353d
Luis Ka <aerop@DESKTOP-N8IN1T1>
2016-04-15 08:36:10 +0200

    Merge LuisKa/master
    Conflicts:
    	src/projecteprogramaciolmp/Jugador.java
    	src/projecteprogramaciolmp/Rei.java
    	src/projecteprogramaciolmp/Reina.java

--- src/projecteprogramaciolmp/Carta.java
+++ src/projecteprogramaciolmp/Carta.java
@@ -2,7 +2,8 @@
 //
 //  Enmagatzema la informació de una carta.
 //  Serveix per verificar si un jugador pot fer realment un rol
-//  
+//  ATRIBUTS: nomCarta (String) que ha de coincidir amb un dels rols que hi ha al 
+//  joc.
 //
 /////////////////////////////////////
 package projecteprogramaciolmp;
@@ -12,5 +13,12 @@ package projecteprogramaciolmp;
  * @author aerop
  */
 public class Carta {
+    String nomCarta;
     
+    public Carta(){
+        nomCarta="defecte";
+    }
+    public Carta(String nomRol){
+        nomCarta= nomRol;
+    }
 }
