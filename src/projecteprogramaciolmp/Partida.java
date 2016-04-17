7e18525c731d97194ff9898956ed78402f852fde
Luis Ka <aerop@DESKTOP-N8IN1T1>
2016-04-17 17:26:17 +0200

    Merge LuisKa/master
    Conflicts:
    	src/projecteprogramaciolmp/Jugador.java
    	src/projecteprogramaciolmp/Rei.java
    	src/projecteprogramaciolmp/Reina.java

--- src/projecteprogramaciolmp/Partida.java
+++ src/projecteprogramaciolmp/Partida.java
@@ -2,13 +2,14 @@
 package projecteprogramaciolmp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Partida {
     // Atributs
     // --------------------
-    private int _monJusticia; // Monedes del palau de justícia
-    private int _monBanc; // Monedes del banc nacional
+    private int _monedesJusticia; // Monedes del palau de justícia
+    private int _monedesBanc; // Monedes del banc nacional
     private int _jugadorActual; // Index de l'array de jugadors que indica el jugador actual.
     private int _monedesPerGuanyar;
     private int _monedesTotals;
@@ -21,20 +22,24 @@ public class Partida {
     // ============================================================
        public Partida() {
       
-           _monJusticia=0;
-           _monBanc=0;
+           _monedesJusticia=0;
+           _monedesBanc= Integer.MAX_VALUE; //al banc mai se li acaben les monedes.
        }
-       
-       public Partida(int nJugadors, int monJusticia,int monBanc) {
+       public void PartidaSettings(int nJugadors, int monedesJusticia){
+           //en el cas que no s'indiquin les monedes per jugador, s'invocara el 
+           //seguent metode que te per defecte 6 monedes per jugador.
+           PartidaSettings(nJugadors,monedesJusticia,6);
+       }
+       public void PartidaSettings(int nJugadors, int monedesJusticia,int monedesPerJugador) {
            _JugadorsQueJuguen= new ArrayList<Jugador> (nJugadors); //array de nJugadors
            /* for (i=0; i< nJugadors; i++){
                 _JugadorsQueJuguen[i].setJugador(nomJugador,monedes); //com coi implementem aixo?
               }
            */
-           _monJusticia=0;
-           _monBanc=_monedesTotals - 6*nJugadors; //el 6 son les monedes per jugador
+           _monedesJusticia=0;
            
-           _mazo= new ArrayList<Carta>(Arrays.asList(new Carta("Rei"),new Carta("Reina"),new Carta("Camperol"),new Carta("Bisbe"),new Carta("Espia"),new Carta("Inquisidor"),new Carta("Jutge"),new Carta("Lladre"),new Carta("Trampos"),new Carta("Viuda"),new Carta("Buffo"),new Carta("Bruixa") ));
+           //inicialització del mazo.
+           _mazo= new ArrayList<>(Arrays.asList(new Carta(new Rei()),new Carta(new Reina()),new Carta(new Camperol()),new Carta(new Camperol()),new Carta(new Bisbe()),new Carta(new Espia()),new Carta(new Inquisidor()),new Carta(new Jutge()),new Carta(new Lladre()),new Carta(new Trampos()),new Carta(new Viuda()),new Carta(new Buffo()),new Carta(new Bruixa()) ));
        }
     
     
@@ -72,7 +77,23 @@ public class Partida {
         public void repartirCartes () {
         // Pre: --
         // Post: Reparteix les cartes als diferents jugadors de la partida
-            
+            /*ESQUEMA:
+                int nCartesPerJugador= 4;
+                if(vectorJugadors.size()==2)
+                    nCartesPerJugador= 3;
+                else if(vectorJugadors.size()==3)
+                    nCartesPerJugador= 2;
+        
+                for(int i= 0; i<nJugadors; i++){
+                    for(int j= 0; j<nCartesPerJugador; j++){
+                        int carta= RANDOM(entre 0 i vectorCartes.size()-1);
+                        
+                        vectorJugador[i].afegirCarta(vectorCartes[carta]);
+                        vectorCartes[carta].erase();
+                        
+        PROBLEMES: No s'hi fixa en les limitacions de la pag. 7 del PDF, cal pensar-lo
+            */
+        
         }
 
         private void descartarCartes () {
