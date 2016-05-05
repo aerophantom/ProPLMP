1e0ddcb0e2f7b7b0b05674529ab57edf59756681
Luis Ka <aerop@DESKTOP-N8IN1T1>
2016-05-05 12:18:48 +0200

    Merge LuisKa/master
    Conflicts:
    	src/projecteprogramaciolmp/Partida.java

--- src/projecteprogramaciolmp/Espia.java
+++ src/projecteprogramaciolmp/Espia.java
@@ -9,26 +9,33 @@
 
 package projecteprogramaciolmp;
 import java.util.ArrayList;
-/**
- *
- * @author aerop
- */
+
 public class Espia extends Rol{
     
      // ============================================================
     // Mètodes CONSTRUCTORS
     // ============================================================
     
-    public Espia() {
+    public Espia() {/**
+     * PRE:--
+     * POST: Seteja el identificador del rol com a "Espia"
+     */
         
         _nomRol="Espia";
     }
     @Override
-    public void accioDeRol(Partida p){
-        ArrayList<Integer> aux= new ArrayList<>();
-        aux= p.escollirJugadors(1);//de moment fico la opcio de nomes poder escollir jugadors,
-        //mes endavant, podra escollir de la taula.
-        int nCarta= p.escollirCarta(p.obtIndexJugadorExecutador());
+    public void accioDeRol(Partida p){/**
+     * PRE: <p> setejat
+     * POST: Selecciona un jugador i una carta d'aquest (o del _mazo) i la veu. 
+     * Despres pot decidir si intercanviar-la o no (intercanvi en fals).
+     * 
+     * NOTA: De moment NO esta implementat la opcio del _mazo.
+     */
+        //RECOLLIDA DADES
+        ArrayList<Integer> aux= new ArrayList<>();                                      //Declaro array auxiliar on guardare l'index del jugador seleccionat
+        aux= p.escollirJugadors(1);                                                     //Guardo al array el index del jugador seleccionat pel jugador executador
+        //la siguiente linea mepa que esta mal ¿pq el parametro es el que es y no el del jugador seleccionado?
+        int nCarta= p.escollirCarta(p.obtIndexJugadorExecutador());                     //Guardo en una variable local l'index de la carta 
         p.descobrirCarta(aux.get(0), nCarta);
         System.out.print("Una vegada vista la carta del jugador, vols fer intercanvi? (si dius no es fara intercanvi fals)");
         if(p.preguntarJugador(p.obtIndexJugadorExecutador())){
